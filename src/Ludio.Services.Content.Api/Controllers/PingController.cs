2abd5d97d9ff778b8d7cbf8389ea681722f52405
Nico Lammers <nico.lammers@uname-it.nl>
2019-06-05 15:12:54 +0200

    Upgraded to .net Core 2.2

---
+++ src/Ludio.Services.Content.Api/Controllers/PingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ludio.Services.Content.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        public string Ping()
+        {
+            return "Pong!";
+        }
+    }
+}
